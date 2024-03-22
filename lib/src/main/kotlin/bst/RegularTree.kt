2e9f9ae6ee908fe97f04f72ba8cf5eaf147ffc01
kinokotakenoko9 <chdmitry2@gmail.com>
2024-03-22 16:15:05 +0300

    fix: update regular tree insert method type

--- lib/src/main/kotlin/bst/RegularTree.kt
+++ lib/src/main/kotlin/bst/RegularTree.kt
@@ -7,11 +7,15 @@ class RegularTree<K : Comparable<K>, V> : RegularAbstractBST<K, V, BSTNode<K, V>
         return null // TODO
     }
 
-    override fun insert(key: K, value: V): Boolean {
-        return false // TODO
+    override fun insert(key: K, value: V): BSTNode<K, V> {
+        return createNode(key, value) // TODO
     }
 
     override fun remove(key: K): V? {
         return null // TODO
+    }
+
+    override fun createNode(key: K, value: V): BSTNode<K, V> {
+        return BSTNode(key, value)
     }
 }
