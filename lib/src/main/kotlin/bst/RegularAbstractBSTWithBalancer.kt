6953cddfd11abe65b1c5ff29759b8035fcf9e852
kinokotakenoko9 <chdmitry2@gmail.com>
2024-03-22 16:22:17 +0300

    fix: remove default value

--- lib/src/main/kotlin/bst/RegularAbstractBSTWithBalancer.kt
+++ lib/src/main/kotlin/bst/RegularAbstractBSTWithBalancer.kt
@@ -6,7 +6,7 @@ import bst.nodes.AbstractBSTNodeWithParent
 abstract class RegularAbstractBSTWithBalancer<K : Comparable<K>, V, R : AbstractBSTNodeWithParent<K, V, R>> : RegularAbstractBST<K, V, R>() {
     abstract var balancer: AbstractBSTBalancer<K, V, R>
 
-    fun balance(balanceFunction: (R) -> Unit, node: R? = root) {
+    fun balance(balanceFunction: (R) -> Unit, node: R?) {
         val balanceNode = node ?: return
         balanceFunction(balanceNode)
     }
