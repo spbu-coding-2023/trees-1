15e2d99a54c876eb7ddac07033cb9e15d3ce9cb1
kinokotakenoko9 <chdmitry2@gmail.com>
2024-03-22 15:57:54 +0300

    fix: return type of insert method set to inserted node

--- lib/src/main/kotlin/bst/AbstractBST.kt
+++ lib/src/main/kotlin/bst/AbstractBST.kt
@@ -5,6 +5,6 @@ import bst.nodes.AbstractBSTNode
 abstract class AbstractBST<K : Comparable<K>, V, R : AbstractBSTNode<K, V, R>> {
     abstract var root: R?
     abstract fun search(key: K): V?
-    abstract fun insert(key: K, value: V): Boolean
+    abstract fun insert(key: K, value: V): R
     abstract fun remove(key: K): V?
 }
