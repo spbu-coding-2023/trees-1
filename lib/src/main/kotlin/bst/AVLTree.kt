94e95610d03712846da138a24a443ae68b9b1577
kinokotakenoko9 <chdmitry2@gmail.com>
2024-03-23 21:21:30 +0300

    add: avl tree implementations

--- lib/src/main/kotlin/bst/AVLTree.kt
+++ lib/src/main/kotlin/bst/AVLTree.kt
@@ -3,17 +3,45 @@ package bst
 import bst.balancers.AbstractBSTBalancer
 import bst.balancers.AVLTreeBalancer
 import bst.nodes.AVLTreeNode
-import bst.nodes.RedBlackTreeNode
+import kotlin.math.max
 
 class AVLTree<K : Comparable<K>, V> : RegularAbstractBSTWithBalancer<K, V, AVLTreeNode<K, V>>() {
     override var balancer: AbstractBSTBalancer<K, V, AVLTreeNode<K, V>> = AVLTreeBalancer()
 
     override fun setNodeRight(nodeParent: AVLTreeNode<K, V>, nodeChild: AVLTreeNode<K, V>?) {
-        TODO("Not yet implemented")
+        when (nodeChild) {
+            null -> {
+                nodeParent.right = null
+            }
+            else -> {
+                nodeChild.parent = nodeParent
+                nodeParent.right = nodeChild
+
+                var currentNode = nodeChild
+                while (currentNode != root) {
+                    currentNode = currentNode?.parent ?: break
+                    currentNode.height = max(currentNode.right?.height ?: 0, currentNode.left?.height ?: 0) + 1
+                }
+            }
+        }
     }
 
     override fun setNodeLeft(nodeParent: AVLTreeNode<K, V>, nodeChild: AVLTreeNode<K, V>?) {
-        TODO("Not yet implemented")
+        when (nodeChild) {
+            null -> {
+                nodeParent.left = null
+            }
+            else -> {
+                nodeChild.parent = nodeParent
+                nodeParent.left = nodeChild
+
+                var currentNode = nodeChild
+                while (currentNode != root) {
+                    currentNode = currentNode?.parent ?: break
+                    currentNode.height = max(currentNode.right?.height ?: 0, currentNode.left?.height ?: 0) + 1
+                }
+            }
+        }
     }
 
     override fun search(key: K): V? {
@@ -22,7 +50,7 @@ class AVLTree<K : Comparable<K>, V> : RegularAbstractBSTWithBalancer<K, V, AVLTr
 
     override fun insert(key: K, value: V): AVLTreeNode<K, V> {
         val newNode = super.insert(key, value)
-        super.balance(balancer::inserter, root)
+        super.balance(balancer::inserter, newNode.parent)
         return newNode // TODO
     }
 
@@ -33,5 +61,14 @@ class AVLTree<K : Comparable<K>, V> : RegularAbstractBSTWithBalancer<K, V, AVLTr
 
     override fun createNode(key: K, value: V): AVLTreeNode<K, V> {
         return AVLTreeNode(key, value, null, 1)
+    }
+
+    override fun setNode(node: AVLTreeNode<K, V>, newNode: AVLTreeNode<K, V>) {
+        node.key = newNode.key
+        node.value = newNode.value
+        node.right = newNode.right
+        node.left = newNode.left
+        node.height = newNode.height // TODO
+        node.parent = newNode.parent
     }
 }
