5b8410f7277cb2478261ef4e612f6b53f631d634
Mukovenkov Roman <roman.mukovenkov@gmail.com>
2024-03-26 17:07:26 +0300

    add: red-black tree remover

--- lib/src/main/kotlin/bst/balancers/RedBlackTreeBalancer.kt
+++ lib/src/main/kotlin/bst/balancers/RedBlackTreeBalancer.kt
@@ -44,8 +44,60 @@ class RedBlackTreeBalancer<K : Comparable<K>, V> : AbstractBSTBalancer<K, V, Red
         }
     }
 
-    
+    private fun RedBlackTreeNode<K, V>.findSibling() : RedBlackTreeNode<K, V>? {
+        when (this) {
+            this.parent.left -> return this.parent.right
+            this.parent.right -> return this.parent.left
+            else -> return null
+        }
+    }
+
     override fun remover(node: RedBlackTreeNode<K, V>) {
-        // TODO
+        if (node.parent != null) {
+            val sibling = node.findSibling()
+            if (sibling.isRed()) {
+                node.parent.setRed()
+                sibling.setBlack()
+                if (node == node.parent.left) {
+                    super.rotateLeft(node.parent)
+                } else {
+                    super.rotateRight(node.parent)
+                }
+            }
+            if (node.parent.isBlack() && sibling.isBlack() && sibling.left.isBlack() && sibling.right.isBlack()) {
+                sibling.setRed()
+                remover(node.parent)
+            } else {
+                if (node.parent.isRed() && sibling.isBlack() && sibling.left.isBlack() && sibling.right.isBlack()) {
+                    sibling.setRed()
+                    node.parent.setBlack()
+                } else {
+                    if (sibling.isBlack()) {
+                        if (node == node.parent.left && sibling.right.isBlack() && sibling.left.isRed()) {
+                            sibling.setRed()
+                            sibling.left.setBlack()
+                            super.rotateRight(sibling)
+                        } else if (node == node.parent.right && sibling.left.isBlack() && sibling.right.isRed()) {
+                            sibling.setRed()
+                            sibling.right.setBlack()
+                            super.rotateLeft(sibling)
+                        }
+                    }
+                    if (node.parent.isRed()) {
+                        sibling.setRed()
+                    } else {
+                        sibling.setBlack()
+                    }
+                    node.parent.setBlack()
+                    if (node == node.parent.left) {
+                        sibling.right.setBlack()
+                        rotateLeft(node.parent)
+                    } else {
+                        sibling.left.setBlack()
+                        rotateRight(node.parent)
+                    }
+                }
+            }
+        }
     }
 }
