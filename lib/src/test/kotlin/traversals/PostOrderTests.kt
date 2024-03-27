ccd9008bc71bf42404294d5c561a86491c65c8dd
Mukovenkov Roman <roman.mukovenkov@gmail.com>
2024-03-27 22:25:27 +0300

    add: all tests for traversals (still needs avl and binary trees' results to be hardcoded)

--- lib/src/test/kotlin/traversals/PostOrderTests.kt
+++ lib/src/test/kotlin/traversals/PostOrderTests.kt
@@ -13,13 +13,11 @@ class PostOrderTraversalTest() {
         val postOrderRB = postOrder<Int, String, RedBlackTreeNode<Int, String>>()
         val postOrderBST = postOrder<Int, String, BSTNode<Int, String>>()
         val postOrderAVL = postOrder<Int, String, AVLTreeNode<Int, String>>()
-        RBTreeResult : List<RedBlackTreeNode<Int, String>> = postOrderRB(RBTree) {node -> node.key}
-        BSTreeResult : List<BSTNode<Int, String>> = postOrderBST(BSTree) {node -> node.value}
-        AVLTreeResult : List<AVLTreeNode<Int, String>> = postOrderAVL(AVLTree) {node -> node.left}
-        assertEquals(RBTreeResult, [6, 1, 11, 8, 15, 22, 27, 25, 13])
+        RBTreeResult : List<RedBlackTreeNode<Int, String>> = postOrderRB.traverse(RBTree.root) {node -> node.key}
+        BSTreeResult : List<BSTNode<Int, String>> = postOrderBST.traverse(BSTree.root) {node -> node.value}
+        AVLTreeResult : List<AVLTreeNode<Int, String>> = postOrderAVL.traverse(AVLTree.root) {node -> node.left}
+        assertEquals(RBTreeResult, [6, 1, 11, 8, 15, 22, 27, 25, 17, 13])
         // assertEquals(BSTreeResult, [your hardcoded result here])
         // assertEquals(AVLTreeResult, [your hardcoded result here])
     }
 }
-
-
--- lib/src/test/kotlin/traversals/PreOrderTests.kt
+++ lib/src/test/kotlin/traversals/PreOrderTests.kt
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Assertions.assertEquals
-import bst.traversals.PostOrder
+import bst.traversals.PreOrder
 import bst.tests
 
 class PreOrderTraversalTest() {
@@ -10,15 +10,14 @@ class PreOrderTraversalTest() {
         val RBTree = setUpBalancedRBTree()
         val BSTree = setUpBalancedBSTree()
         val AVLTree = setUpBalancedAVLTree()
-        val preOrderRB = postOrder<Int, String, RedBlackTreeNode<Int, String>>()
-        val preOrderBST = postOrder<Int, String, BSTNode<Int, String>>()
-        val preOrderAVL = postOrder<Int, String, AVLTreeNode<Int, String>>()
-        RBTreeResult : List<RedBlackTreeNode<Int, String>> = preOrderRB(RBTree) {node -> node.key}
-        BSTreeResult : List<BSTNode<Int, String>> = preOrderBST(BSTree) {node -> node.value}
-        AVLTreeResult : List<AVLTreeNode<Int, String>> = preOrderAVL(AVLTree) {node -> node.left}
-        assertEquals(RBTreeResult, [6, 1, 11, 8, 15, 22, 27, 25, 13])
+        val preOrderRB = preOrder<Int, String, RedBlackTreeNode<Int, String>>()
+        val preOrderBST = preOrder<Int, String, BSTNode<Int, String>>()
+        val preOrderAVL = preOrder<Int, String, AVLTreeNode<Int, String>>()
+        RBTreeResult : List<RedBlackTreeNode<Int, String>> = preOrderRB.traverse(RBTree.root) {node -> node.key}
+        BSTreeResult : List<BSTNode<Int, String>> = preOrderBST.traverse(BSTree.root) {node -> node.value}
+        AVLTreeResult : List<AVLTreeNode<Int, String>> = preOrderAVL.traverse(AVLTree.root) {node -> node.left}
+        assertEquals(RBTreeResult, [13, 8, 1, 6, 11, 17, 15, 25, 22, 27])
         // assertEquals(BSTreeResult, [your hardcoded result here])
         // assertEquals(AVLTreeResult, [your hardcoded result here])
     }
 }
-
