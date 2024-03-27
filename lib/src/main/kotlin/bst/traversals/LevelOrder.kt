d3fb547239c32ddd288ceab490265529bab6af33
kinokotakenoko9 <chdmitry2@gmail.com>
2024-03-27 18:42:20 +0300

    fix: add missing return

--- lib/src/main/kotlin/bst/traversals/LevelOrder.kt
+++ lib/src/main/kotlin/bst/traversals/LevelOrder.kt
@@ -1,26 +1,35 @@
 package bst.traversals
 
 import bst.nodes.AbstractBSTNode
+import java.util.ArrayDeque
 
 class LevelOrder<K : Comparable<K>, V, R : AbstractBSTNode<K, V, R>> : BSTTraversal<K, V, R> {
-    override fun <T> traverse(node: R, extractionFunction: (R) -> T): List<T> {
+    override fun <T> traverse(
+        node: R,
+        extractionFunction: (R) -> T,
+    ): List<T> {
         return levelOrderTraversal(node, extractionFunction, mutableListOf())
     }
 
-    private fun <T> levelOrderTraversal(node: R, extractionFunction : (R) -> T, result: MutableList<T>): List<T> {
+    private fun <T> levelOrderTraversal(
+        node: R,
+        extractionFunction: (R) -> T,
+        result: MutableList<T>,
+    ): List<T> {
         if (node != null) {
             val queue = ArrayDeque<R>()
             queue.add(node)
             while (queue.isNotEmpty()) {
                 val currentNode = queue.poll()
                 result.add(extractionFunction(currentNode))
-                if currentNode.left != null {
+                if (currentNode.left != null) {
                     queue.add(currentNode.left)
                 }
-                if currentNode.right != null {
+                if (currentNode.right != null) {
                     queue.add(currentNode.right)
                 }
             }
         }
+        return result
     }
 }
