f02a46f629a5984d57d1ae9e033197fa505ca322
Mukovenkov Roman <roman.mukovenkov@gmail.com>
2024-03-27 17:20:15 +0300

    add: inOrder, levelOrder, postOrder traversals

--- lib/src/main/kotlin/bst/traversals/LevelOrder.kt
+++ lib/src/main/kotlin/bst/traversals/LevelOrder.kt
@@ -4,6 +4,23 @@ import bst.nodes.AbstractBSTNode
 
 class LevelOrder<K : Comparable<K>, V, R : AbstractBSTNode<K, V, R>> : BSTTraversal<K, V, R> {
     override fun <T> traverse(node: R, extractionFunction: (R) -> T): List<T> {
-        return listOf(extractionFunction(node)) // TODO
+        return levelOrderTraversal(node, extractionFunction, mutableListOf())
+    }
+
+    private fun <T> levelOrderTraversal(node: R, extractionFunction : (R) -> T, result: MutableList<T>): List<T> {
+        if (node != null) {
+            val queue = ArrayDeque<R>()
+            queue.add(node)
+            while (queue.isNotEmpty()) {
+                val currentNode = queue.poll()
+                result.add(extractionFunction(currentNode))
+                if currentNode.left != null {
+                    queue.add(currentNode.left)
+                }
+                if currentNode.right != null {
+                    queue.add(currentNode.right)
+                }
+            }
+        }
     }
 }
