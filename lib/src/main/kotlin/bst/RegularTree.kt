e21695542d67a7dc1ac58bde030fcf4b9b9e14f9
kinokotakenoko9 <chdmitry2@gmail.com>
2024-03-29 21:03:58 +0300

    add: regular tree methods implementation

--- lib/src/main/kotlin/bst/RegularTree.kt
+++ lib/src/main/kotlin/bst/RegularTree.kt
@@ -4,18 +4,18 @@ import bst.nodes.BSTNode
 
 class RegularTree<K : Comparable<K>, V> : RegularAbstractBST<K, V, BSTNode<K, V>>() {
     override fun search(key: K): V? {
-        return null // TODO
+        return super.search(key)
     }
 
     override fun insert(
         key: K,
         value: V,
     ): BSTNode<K, V> {
-        return createNode(key, value) // TODO
+        return super.insert(key, value)
     }
 
     override fun remove(key: K): V? {
-        return null // TODO
+        return super.remove(key)
     }
 
     override fun createNode(
@@ -29,20 +29,23 @@ class RegularTree<K : Comparable<K>, V> : RegularAbstractBST<K, V, BSTNode<K, V>
         nodeParent: BSTNode<K, V>,
         nodeChild: BSTNode<K, V>?,
     ) {
-        TODO("Not yet implemented")
+        nodeParent.left = nodeChild?.left
     }
 
     override fun setNodeRight(
         nodeParent: BSTNode<K, V>,
         nodeChild: BSTNode<K, V>?,
     ) {
-        TODO("Not yet implemented")
+        nodeParent.right = nodeChild?.right
     }
 
     override fun setNode(
         node: BSTNode<K, V>,
         newNode: BSTNode<K, V>,
     ) {
-        TODO("Not yet implemented")
+        node.key = newNode.key
+        node.value = newNode.value
+        node.left = newNode.left
+        node.right = newNode.right
     }
 }
