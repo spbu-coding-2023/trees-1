db1d41f51237b4b206a0d842d913e0a02be775d8
kinokotakenoko9 <chdmitry2@gmail.com>
2024-03-30 14:11:03 +0300

    add: simple insert tests

--- lib/src/test/kotlin/trees/AVLTreeTest.kt
+++ lib/src/test/kotlin/trees/AVLTreeTest.kt
@@ -21,6 +21,27 @@ class AVLTreeTest {
         tree.insert(23, "Germany")
 
         assertEquals("Germany", tree.search(23))
+        assertNull(tree.search(69))
+    }
+
+    @Test
+    fun `search two nodes`() {
+        tree.insert(23, "Germany")
+        tree.insert(35, "Niger")
+
+        assertEquals("Germany", tree.search(23))
+        assertEquals("Niger", tree.search(35))
+    }
+
+    @Test
+    fun `search three nodes`() {
+        tree.insert(23, "Germany")
+        tree.insert(35, "Niger")
+        tree.insert(12, "Moldova")
+
+        assertEquals("Germany", tree.search(23))
+        assertEquals("Moldova", tree.search(12))
+        assertEquals("Niger", tree.search(35))
     }
 
     @Test
