a86daf1083020fb091fbc0b8e57f1a73e4982e97
kinokotakenoko9 <chdmitry2@gmail.com>
2024-04-07 22:04:49 +0300

    add: proper visibility modifiers

--- lib/src/main/kotlin/bst/nodes/AbstractBSTNodeWithParent.kt
+++ lib/src/main/kotlin/bst/nodes/AbstractBSTNodeWithParent.kt
@@ -3,5 +3,7 @@ package bst.nodes
 abstract class AbstractBSTNodeWithParent<K : Comparable<K>, V, R : AbstractBSTNodeWithParent<K, V, R>>(
     key: K,
     value: V,
-    var parent: R?,
-) : AbstractBSTNode<K, V, R>(key, value)
+    internal var parent: R?,
+) : AbstractBSTNode<K, V, R>(key, value) {
+    fun getParent() = parent
+}
--- lib/src/main/kotlin/bst/traversals/LevelOrder.kt
+++ lib/src/main/kotlin/bst/traversals/LevelOrder.kt
@@ -3,7 +3,7 @@ package bst.traversals
 import bst.nodes.AbstractBSTNode
 import java.util.ArrayDeque
 
-class LevelOrder<K : Comparable<K>, V, R : AbstractBSTNode<K, V, R>> : BSTTraversal<K, V, R> {
+internal class LevelOrder<K : Comparable<K>, V, R : AbstractBSTNode<K, V, R>> : BSTTraversal<K, V, R> {
     override fun <T> traverse(
         node: R?,
         extractionFunction: (R) -> T,
